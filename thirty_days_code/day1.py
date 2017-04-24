cf48058e786d926225acf24570fe561b7de4d098
Amanda Morris <amandafmorris@gmail.com>
2017-04-24 00:31:41 +0000

    day1 done

--- thirty_days_code/day1.py
+++ thirty_days_code/day1.py
@@ -1,11 +1,16 @@
 
 # Declare second integer, double, and String variables.
+# variables don't have to be declared before assigning in python
 
 # Read and save an integer, double, and String to your variables.
-
+my_int = int(raw_input())
+my_double = float(raw_input())
+my_string = raw_input()
 # Print the sum of both integer variables on a new line.
-
+print i + my_int
 # Print the sum of the double variables on a new line.
-
+print d + my_double
 # Concatenate and print the String variables on a new line
 # The 's' variable above should be printed first.
+my_string = s + my_string
+print my_string
