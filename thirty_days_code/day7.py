66489694bf22dc724def74cb7ae126366fa76246
Amanda Morris <amandafmorris@gmail.com>
2017-05-02 00:15:09 +0000

    day7 done

---
+++ thirty_days_code/day7.py
@@ -0,0 +1,11 @@
+#!/bin/python
+"""print array in reverse order, on a single line"""
+
+import sys
+
+
+n = int(raw_input().strip())
+arr = map(int,raw_input().strip().split(' '))
+
+for num in arr[::-1]:
+    print num,
