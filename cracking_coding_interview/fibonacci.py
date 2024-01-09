dca906954b41539bc6e11237637314ab55c2bfe3
amandamorris <amandafmorris@gmail.com>
2024-01-09 11:59:25 -0800

    Update fibonacci.py

--- cracking_coding_interview/fibonacci.py
+++ cracking_coding_interview/fibonacci.py
@@ -2,8 +2,10 @@ def fibonacci(n):
     # Write your code here.
     fibs = [0, 1]
     for i in range(2, 41):
+        # adding a comment
         fibs.append(fibs[i-1]+fibs[i-2])
     return fibs[n]
+    
 
 
 
